9c83fc152b923489b14fc6395e271a9357c518d1
Amit88k <amit88khandelwal@gmail.com>
2019-01-22 22:05:41 +0530

    changes updated

---
+++ examples/01_scheduling_first_pipeline/setting_up_dependencies_among_tasks.py
@@ -0,0 +1,27 @@
+We have tasks t1, t2 and t3 in tutorial.py that do not depend on each other. Here’s a few ways you can define dependencies between them:
+
+t1.set_downstream(t2)
+
+# This means that t2 will depend on t1
+# running successfully to run.
+# It is equivalent to:
+t2.set_upstream(t1)
+
+# The bit shift operator can also be
+# used to chain operations:
+t1 >> t2
+
+# And the upstream dependency with the
+# bit shift operator:
+t2 << t1
+
+# Chaining multiple dependencies becomes
+# concise with the bit shift operator:
+t1 >> t2 >> t3
+
+# A list of tasks can also be set as
+# dependencies. These operations
+# all have the same effect:
+t1.set_downstream([t2, t3])
+t1 >> [t2, t3]
+[t2, t3] << t1
